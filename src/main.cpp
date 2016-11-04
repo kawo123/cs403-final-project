5430f08eb5f33ec4d440ad91dc3c565a98842266
kawo123 <kwfong@umass.edu>
2016-11-04 17:20:58 -0400

    initial scaffolding

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sstream>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "compsci403_final");
+  ros::NodeHandle n;
+
+
+
+  ros::spin();
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
