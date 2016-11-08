641aa54f7ed4c22c36cac7406f03c1f52380e9e2
desporous <carolineyu@umass.edu>
2016-11-08 16:47:24 -0500

    added two methods with comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,17 @@
 #include <std_msgs/String.h>
 #include <sstream>
 
+bool checkCylinder(){//given 4 points
+	//for displaying cylinder on rviz, use marker
+	//checks if cylinder radius is acceptable and whether or not the line corresponding with the cylinder intercepts the projector plane (rviz projector plane)
+	//if yes, return true
+	//if not return false
+}
+
+void bestCylinder(){
+	//finds best fit cylinder for filtered point cloud
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "compsci403_final");
   ros::NodeHandle n;
